c07e8dc79853f51f60cacbc92cd88ccec7ff4814
Daniel Huffman <163620129+Drhuffman123@users.noreply.github.com>
2026-09-22 18:07:38 -0400

    Update lasagna_spec.rb, add test 2

--- spec/lasagna_spec.rb
+++ spec/lasagna_spec.rb
@@ -6,7 +6,11 @@ require 'minitest/autorun'
 require_relative '../lib/lasagna.rb'
 
 class LasagnaTest < Minitest::Test
-  def test_expected_mins_in_oven
+  def test_1
     assert_equal 40, Lasagna::EXPECTED_MINUTES_IN_OVEN
+  end
+
+  def test_2
+    assert_equal 15, Lasagna.new.remaining_minutes_in_oven(25)
   end
 end
