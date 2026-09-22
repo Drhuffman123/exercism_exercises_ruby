0452cd9f8a0f48c1e7cba75b424aab23225333d5
Daniel Huffman <163620129+Drhuffman123@users.noreply.github.com>
2026-09-22 18:00:40 -0400

    Update lasagna_spec.rb

--- spec/lasagna_spec.rb
+++ spec/lasagna_spec.rb
@@ -1,1 +1,12 @@
 # lasagna_spec.rb
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+require_relative '../lib/lasagna.rb'
+
+class LasagnaTest < Minitest::Test
+  def test_expected_mins_in_oven
+    assert_equal 40, Lasagna::EXPECTED_MINUTES_IN_OVEN
+  end
+end
